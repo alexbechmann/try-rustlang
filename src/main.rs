f9d36e9e1f85cd53d680c648acfafedc7aa7f59f
Alex <alex.bechmann@outlook.com>
2023-07-14 10:04:58 +0200

    feat: anyof and allof types

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate serde_derive;
 extern crate serde_json;
 mod message;
 mod utils;
+mod kafka_handler;
+
 use serde_json::json;
 use utils::add::add;
 
@@ -11,6 +13,8 @@ use utils::add::add;
 fn main() {
     let source = "rust";
     println!("source is {source}");
+
+    kafka_handler::subscribe::subscribe();
 
     let json_value = json!({
         "id": "1", 
@@ -20,11 +24,35 @@ fn main() {
         "type": "example.message" ,
         "data": {
             "greeting": "Hello World!",
-            "style": "full"
+            "style": "full",
+            "thing1": false,
+            "thing2": {
+                "foo": "foo",
+                "bar": "bar"
+            }
         }
     });
 
     let message: message::Message = serde_json::from_str(&json_value.to_string()).unwrap();
+    
+    match &message.data.thing1 {
+        message::Thing1Union::Bool(value) => {
+            // Code to handle Bool variant
+            println!("Bool value: {}", value);
+        }
+        message::Thing1Union::Double(value) => {
+            // Code to handle Double variant
+            println!("Double value: {}", value);
+        }
+        message::Thing1Union::String(value) => {
+            // Code to handle String variant
+            println!("String value: {}", value);
+        }
+        message::Thing1Union::Thing1Class(thing1_class) => {
+            // Code to handle Thing1Class variant
+            println!("Thing1Class foo value: {}", thing1_class.foo);
+        }
+    }
 
     let message_new = message::Message{
         id: String::from("id"),
@@ -38,9 +66,14 @@ fn main() {
         specversion: String::from("specversion"),
         data: message::Data {
             greeting: String::from("Hello World!"),
-            style: message::Style::Full
+            style: message::Style::Full,
+            thing1: message::Thing1Union::Thing1Class(message::Thing1Class {
+                foo: String::from("id"),
+            }),
+            thing2: message::Thing2{foo: String::from("foo"), bar: String::from("bar")}
         }
     };
+
     
     let serialized_access = serde_json::to_string(&message_new).unwrap();
     
