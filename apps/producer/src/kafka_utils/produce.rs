39bbf62a74ba80d80f3b094d92b4ba7a92ff69c8
Alex Bechmann <alex.bechmann@velux.com>
2023-07-20 16:51:23 +0200

    refactor: message field syntax

--- apps/producer/src/kafka_utils/produce.rs
+++ apps/producer/src/kafka_utils/produce.rs
@@ -11,30 +11,31 @@ pub fn produce() {
     println!("Starting producer...");
 
     for i in 0..i32::MAX {
-        let purchase = protos::purchase::PurchaseCloudEvent {
+        let purchase_event = protos::purchase::PurchaseCloudEvent {
             id: String::from(format_f!("id-{i}")),
             source: String::from("try-rustlang-producer"),
             spec_version: String::from("0.1.0"),
             special_fields: SpecialFields::new(),
             type_: protos::purchase::purchase_cloud_event::Type::PURCHASE.into(),
             time: String::from("time"),
-            data: Some(protos::purchase::purchase_cloud_event::Data {
+            data: protobuf::MessageField::some(protos::purchase::purchase_cloud_event::Data {
                 amount: 12.0,
                 customer_id: String::from(format_f!("customer-{i}")),
                 item: Some(String::from("item1")),
                 special_fields: SpecialFields::new(),
-            })
-            .into(),
+            }),
         };
         let customer_cloud_event = protos::customer_event::CustomerCloudEvent {
             special_fields: SpecialFields::new(),
             payload: Some(
-                protos::customer_event::customer_cloud_event::Payload::Purchase(purchase.clone()),
+                protos::customer_event::customer_cloud_event::Payload::Purchase(
+                    purchase_event.clone(),
+                ),
             ),
         };
         let value = protobuf::Message::write_to_bytes(&customer_cloud_event).unwrap();
         producer.send(&Record::from_value(topic, value)).unwrap();
-        println_f!("Produced message: {purchase.id}");
+        println_f!("Produced message: {purchase_event.id}");
         thread::sleep(Duration::from_secs(3)); // Simulating work
     }
 }
