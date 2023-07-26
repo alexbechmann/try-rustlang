476b4e81b828dec03bece88e4a64fa86c78e8fec
Alex Bechmann <alex.bechmann@velux.com>
2023-07-26 08:48:40 +0000

    feat: produce page view events

--- apps/producer/src/kafka_utils/produce.rs
+++ apps/producer/src/kafka_utils/produce.rs
@@ -3,14 +3,14 @@ use chrono;
 use kafka::producer::{Producer, Record};
 use protobuf::SpecialFields;
 use std::{thread, time::Duration};
-use utils::customer_event;
 use utils::kafka::topics::MESSAGES_TOPIC;
-use utils::purchase;
+use utils::{convert_chrono_to_timestamp, purchase};
+use utils::{customer_event, page_view};
 
-pub fn produce() {
+pub fn start_producing_purchase_events() {
     let brokers = vec![config::CONFIG.kafka_brokers.to_string()];
     let mut producer = Producer::from_hosts(brokers).create().unwrap();
-    println!("Starting producer...");
+    println!("Starting purchase events producer...");
 
     for i in 0..i32::MAX {
         let purchase_event = purchase::PurchaseCloudEvent {
@@ -40,6 +40,44 @@ pub fn produce() {
             .send(&Record::from_value(MESSAGES_TOPIC, value))
             .unwrap();
         println!("Produced message: {}", purchase_event.id);
+        thread::sleep(Duration::from_secs(3)); // Simulating work
+    }
+}
+
+pub fn start_producing_page_view_events() {
+    let brokers = vec![config::CONFIG.kafka_brokers.to_string()];
+    let mut producer = Producer::from_hosts(brokers).create().unwrap();
+    println!("Starting page view producer...");
+
+    for i in 0..i32::MAX {
+        let page_view_event = page_view::PageViewCloudEvent {
+            id: String::from("id"),
+            source: String::from("source"),
+            spec_version: String::from("0.1.0"),
+            special_fields: SpecialFields::new(),
+            type_: page_view::page_view_cloud_event::Type::EXAMPLE_CUSTOMER_PAGE_VIEW.into(),
+            time: protobuf::MessageField::some(
+                convert_chrono_to_timestamp::convert_chrono_to_timestamp(&chrono::Utc::now()),
+            ),
+            data: protobuf::MessageField::some(page_view::page_view_cloud_event::Data {
+                customer_id: String::from("customer1"),
+                url: String::from("http://example.com"),
+                is_special: false,
+                special_fields: SpecialFields::new(),
+            }),
+        };
+        let customer_cloud_event = customer_event::CustomerCloudEvent {
+            special_fields: SpecialFields::new(),
+            payload: Some(customer_event::customer_cloud_event::Payload::PageView(
+                page_view_event.clone(),
+            )),
+        };
+        let value = protobuf::Message::write_to_bytes(&customer_cloud_event).unwrap();
+        let value = protobuf::Message::write_to_bytes(&page_view_event).unwrap();
+        producer
+            .send(&Record::from_value(MESSAGES_TOPIC, value))
+            .unwrap();
+        println!("Produced message: {}", page_view_event.id);
         thread::sleep(Duration::from_secs(3)); // Simulating work
     }
 }
