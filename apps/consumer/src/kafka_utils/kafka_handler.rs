0bd6a099b29a4d3b90ff3f6b3e2179a820d1998e
Alex Bechmann <alex.bechmann@velux.com>
2023-07-26 22:36:27 +0000

    feat: kafka handler tests

--- apps/consumer/src/kafka_utils/kafka_handler.rs
+++ apps/consumer/src/kafka_utils/kafka_handler.rs
@@ -90,3 +90,93 @@ impl KafkaHandlerImpl {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::store::store::MockStore;
+    use chrono::Utc;
+    use protobuf::SpecialFields;
+    use utils::{convert_chrono_to_timestamp::convert_chrono_to_timestamp, page_view, purchase};
+
+    #[tokio::test]
+    async fn handle_purchase_event_test() {
+        let mut store = MockStore::new();
+        store
+            .expect_update_balance()
+            .times(1)
+            .returning(|purchase_event| {
+                println!(
+                    "Mocked update_balance for {}",
+                    purchase_event.data.customer_id
+                );
+                return Ok(true);
+            });
+        let customer_event = customer_event::CustomerCloudEvent {
+            special_fields: SpecialFields::new(),
+            payload: Some(customer_event::customer_cloud_event::Payload::Purchase(
+                purchase::PurchaseCloudEvent {
+                    id: String::from("id"),
+                    source: String::from("source"),
+                    spec_version: String::from("0.1.0"),
+                    special_fields: SpecialFields::new(),
+                    type_: purchase::purchase_cloud_event::Type::EXAMPLE_CUSTOMER_PURCHASE.into(),
+                    time: protobuf::MessageField::some(convert_chrono_to_timestamp(&Utc::now())),
+                    data: protobuf::MessageField::some(purchase::purchase_cloud_event::Data {
+                        amount: 12.0,
+                        customer_id: String::from("customer1"),
+                        item: Some(String::from("item1")),
+                        special_fields: SpecialFields::new(),
+                    }),
+                },
+            )),
+        };
+        let serialized = protobuf::Message::write_to_bytes(&customer_event).unwrap();
+        let kafka_handler = KafkaHandlerImpl {
+            store: Arc::new(store),
+        };
+        let _result = kafka_handler.handle_event(&serialized).await;
+        assert!(true);
+    }
+
+    #[tokio::test]
+    async fn handle_page_view_event_test() {
+        let mut store = MockStore::new();
+        store
+            .expect_increment_page_view()
+            .times(1)
+            .returning(|page_view_event| {
+                println!(
+                    "Mocked increment_page_view for {}",
+                    page_view_event.data.customer_id
+                );
+                return Ok(true);
+            });
+        let customer_event = customer_event::CustomerCloudEvent {
+            special_fields: SpecialFields::new(),
+            payload: Some(customer_event::customer_cloud_event::Payload::PageView(
+                page_view::PageViewCloudEvent {
+                    id: String::from("id"),
+                    source: String::from("source"),
+                    spec_version: String::from("0.1.0"),
+                    special_fields: SpecialFields::new(),
+                    type_: page_view::page_view_cloud_event::Type::EXAMPLE_CUSTOMER_PAGE_VIEW
+                        .into(),
+                    time: protobuf::MessageField::some(convert_chrono_to_timestamp(&Utc::now())),
+                    data: protobuf::MessageField::some(page_view::page_view_cloud_event::Data {
+                        customer_id: String::from("customer1"),
+                        url: String::from("http://example.com"),
+                        is_special: false,
+                        special_fields: SpecialFields::new(),
+                    }),
+                },
+            )),
+        };
+        let serialized = protobuf::Message::write_to_bytes(&customer_event).unwrap();
+        let kafka_handler = KafkaHandlerImpl {
+            store: Arc::new(store),
+        };
+        let _result = kafka_handler.handle_event(&serialized).await;
+        assert!(true);
+    }
+}
